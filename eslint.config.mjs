0d4f691bd25ec611039812f81963f1f4ddc63d1a
Sophia <sashtokalova@gmail.com>
2026-01-18 01:51:52 +0200

    fix: move lint config to cli

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
-
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-    },
 
     ignores: ["**/*"],
   },
 ];
-
-export default eslintConfig;
